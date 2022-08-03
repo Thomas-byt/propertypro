4759b6298cfa19390e615d025bf4f9852e0b480e
Your Name <you@example.com>
2022-08-03 14:28:48 -0700

    Added Data Project

---
+++ src/PropertyPro.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PropertyPro.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
