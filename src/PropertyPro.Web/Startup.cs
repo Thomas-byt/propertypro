38920227ea96ebd3573235912f5ca6169a7f3c9d
Your Name <you@example.com>
2022-08-04 16:03:14 -0700

    added connection string

--- src/PropertyPro.Web/Startup.cs
+++ src/PropertyPro.Web/Startup.cs
@@ -23,6 +23,8 @@ namespace PropertyPro.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AuthenticationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection")));
             services.AddControllersWithViews();
         }
 
